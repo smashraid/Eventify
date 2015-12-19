ccdd2fa9288f6bbde000522f957b18245b300e82
Saulo Tsuchida Cruz <smashraid@gmail.com>
2015-12-18 21:06:28 -0500

    Initial Project

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -8,10 +8,11 @@
  * Controller of the eventifyApp
  */
 angular.module('eventifyApp')
-  .controller('MainCtrl', function () {
+  .controller('MainCtrl', function ($scope) {
     this.awesomeThings = [
       'HTML5 Boilerplate',
       'AngularJS',
       'Karma'
     ];
+    $scope.user = "John Doe";
   });
